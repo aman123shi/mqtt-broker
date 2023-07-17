f4989ea231c26c80c99b157a110640d9b1c0babb
Amanuel Shiferaw <amanuel.shiferaw@addissoftware.com>
2023-07-17 10:38:07 +0300

    refactor : add space for readability

--- app.js
+++ app.js
@@ -2,12 +2,15 @@ const aedes = require("aedes")();
 const server = require("net").createServer(aedes.handle);
 const httpServer = require("http").createServer();
 const ws = require("websocket-stream");
+
 MQTT_Port = 1884;
 const wsPort = 8884;
+
 server.listen(MQTT_Port, function () {
   console.log("Aedes MQTT server started and listening on port", MQTT_Port);
 }),
   ws.createServer({ server: httpServer }, aedes.handle);
+
 httpServer.listen(wsPort, function () {
   console.log("websocket server listening on port ", wsPort);
 });
